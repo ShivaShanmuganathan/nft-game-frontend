7abd0c66bc81a44fcb5d4c4d8d48ce2a348a3841
Shiva Shanmuganathan <shiva_shanmuganathan@srmuniv.edu.in>
2021-11-11 07:00:27 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x99B5E1255a7C09Fe1D24FC358F8E63CaF9D27Ae2';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData};
